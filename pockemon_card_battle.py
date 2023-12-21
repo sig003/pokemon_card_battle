e980fa7da3bd07d7383a35e70546e59b51072ac4
sig03 <sig03@naver.com>
2023-12-21 17:27:23 +0900

    main 파일명 변경

--- pockemon_card_battle.py
+++ pockemon_card_battle.py
@@ -7,11 +7,10 @@ screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption('Pokemon Card Battle!!')
 clock = pygame.time.Clock()
 
-
 while True:
     screen.fill(WHITE)
 
-    event = pygame.event.poll() #이벤트 처리
+    event = pygame.event.poll()
     if event.type == pygame.QUIT:
         break
 
@@ -21,7 +20,9 @@ while True:
     pygame.draw.line(screen, BLACK, (0, 300), (SCREEN_SIZE[0]//4, 300), 1)
     pygame.draw.line(screen, BLACK, (SCREEN_SIZE[0]*3//4, 300), (SCREEN_SIZE[0], 300), 1)
 
-    pygame.display.update() #모든 화면 그리기 업데이트
+    pygame.draw.rect(screen, BLACK, (100, 100, 200, 150))
+
+    pygame.display.update()
     clock.tick(30)
 
 pygame.quit()
